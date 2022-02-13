9f7e63d85132ab0fcd32fe94bf05299bf7d79fa3
hga50 <hga50@sfu.ca>
2022-02-12 16:32:52 -0800

    added requirements.txt

--- db/config.py
+++ db/config.py
@@ -1,1 +1,1 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'postgresql://hga50:12345678@restaurant-db.cj7sflyjpv0c.us-west-2.rds.amazonaws.com:5432/restaurant-db'
