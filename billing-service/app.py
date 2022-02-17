4fa9915dbbfbc974fb1a6915fcaf1dd0a9fa487d
akg38 <akg38@sfu.ca>
2022-02-16 21:06:29 -0800

    Added connection to Database. Modified apis to add respective functionalities.

--- billing-service/app.py
+++ billing-service/app.py
@@ -1,13 +1,48 @@
-from flask import Flask
+from flask import Flask, make_response, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hga50:12345678@restaurant-db.cj7sflyjpv0c.us-west-2.rds.amazonaws.com:5432/restaurant_db'
+db = SQLAlchemy(app)
 
-@app.route('/bill')
+class Order(db.Model):
+    __tablename__ = 'order'
+
+    order_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Text)
+    amount = db.Column(db.Integer)
+    paid = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+@app.route('/bill', methods=['GET'])
 def generate_bill():
-    amt = 0
-    return amt
+    user = request.args.get('user_id')
+    order = get_user_data(user)
+
+    return make_response(order, 200)
     
   
-@app.route('/pay')
+@app.route('/pay', methods=['GET'])
 def make_payment():
-    return 'Payment Done'
+    user = request.args.get('user_id')
+    data = Order.query.filter_by(user_id=user).update({Order.paid: True})
+    db.session.commit()
+    order = get_user_data(user)
+
+    return make_response(order, 200)
+
+def get_user_data(user):
+    data = Order.query.filter_by(user_id=user).first()
+    order = {
+        "user_id":data.user_id,
+        "amount":data.amount,
+        "paid":data.paid
+    }
+
+    return order
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
