3b5cdac7c47c412fc932e6e35982f3d0e11cf32e
asa413 <97705166+asa413@users.noreply.github.com>
2022-02-19 13:31:33 -0800

    Created Menu.py and modified app.py

--- menu_service/app.py
+++ menu_service/app.py
@@ -2,28 +2,17 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 import json
+from menu import Menu 
+from order import Order   
+from db.config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hga50:12345678@restaurant-db.cj7sflyjpv0c.us-west-2.rds.amazonaws.com:5432/restaurant_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret string'
-
-
-class Menu(db.Model):
-    _tablename_ = 'menu'
-    m_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-
-    def _init_(self, pname, color):
-        self.name = pname
-        self.price = color
-
-
-
 
 @app.route('/getMenuItems')
 def get_all_menu_data():
@@ -36,25 +25,6 @@ def get_all_menu_data():
         } for m in menu_items]
     return {"menu_items": results}
 
-
-
-
-class Order(db.Model):
-    _tablename_ = 'order'
-    order_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(80), nullable=True)
-    amount = db.Column(db.Integer, nullable=True)
-    paid = db.Column(db.Boolean, nullable=True)
-
-    def _init_(self, user_id, amount, paid):
-        self.user_id = user_id
-        self.amount = amount
-        self.paid = paid
-
-
-
-
-
 @app.route('/takeOrder', methods=['POST'])
 def take_order():
 
@@ -64,16 +34,9 @@ def take_order():
     
     total_price=0
     for order_obj in order_list:
-        # {
-        #     "id": 1,
-        #     "name": "aa",
-        #     "price": 12.0,
-        #     "qty": 1
-        # },
         price = order_obj['price']
         qty = order_obj['qty']
         total_price += (price * qty)
-    # print(order)
 
     _order_db = Order(user_id = user_id,
                     amount = total_price,
@@ -82,17 +45,12 @@ def take_order():
     db.session.add(_order_db)   
     db.session.commit()
 
-
     order_success = {
         "status": True,
         "message": "Order accepted",
         "total_amount": total_price
     }
     return jsonify(order_success)
-
-
-
-
 
 if __name__ == '__main__':
     db.create_all()
