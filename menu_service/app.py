a8d9cafdd926c15171b6a8c0463fe2fc967f546e
Himank Goel <himankgoel03@gmail.com>
2022-02-19 15:44:10 -0800

    fixed incorrect order (#15)
    Co-authored-by: hga50 <hga50@sfu.ca>

--- menu_service/app.py
+++ menu_service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, make_response, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 import json
@@ -7,11 +7,6 @@ from order import Order
 from db.config import app
 
 db = SQLAlchemy(app)
-
-@app.route('/getMenuItems')
-def get_all_menu_data():
-    return get_menu_data()
-
 
 def get_menu_data():
     menu_items = Menu.query.all()
@@ -22,21 +17,36 @@ def get_menu_data():
             "price": m.price
         } for m in menu_items]
     return {"menu_items": results}
+    
+@app.route('/getMenuItems')
+def get_all_menu_data():
+    return get_menu_data()
 
 @app.route('/takeOrder', methods=['POST'])
 def take_order():
-    res_data = get_menu_data()
-    order = json.loads(request.data)
+    menu_data = get_menu_data()
+    order = json.loads(json.loads(request.data))
     user_id = order['user_id']
     order_list = order['order_list']    
     
     total_price=0
+    
     for selected_o in order_list:
-        for res_o in res_data["menu_items"]:
+        for res_o in menu_data["menu_items"]:
+            item_found = False
             if int(selected_o['id']) == int(res_o['mid']):
+                item_found = True
                 qty = selected_o['qty']
                 price = res_o['price']
                 total_price += (price * qty)
+        
+        if item_found == False:
+            order_failure = {
+                    "status": False,
+                    "message": "Order Declined"
+                }
+
+            return make_response(jsonify(order_failure), 422)
 
     _order_db = Order(user_id = user_id,
                     amount = total_price,
@@ -50,6 +60,7 @@ def take_order():
         "message": "Order accepted",
         "total_amount": total_price
     }
+
     return jsonify(order_success)
 
 if __name__ == '__main__':
