7fa29b6c45b75d892de6f63c08d846196ec42584
akg38 <97567585+akg38@users.noreply.github.com>
2022-02-19 13:38:35 -0800

    Billing error handling (#11)
    * added error handling when bill has already been paid.
    
    * code refactoring
    
    * added code refactoring
    
    * added code refactoring
    
    Co-authored-by: hga50 <hga50@sfu.ca>

--- billing-service/app.py
+++ billing-service/app.py
@@ -1,44 +1,29 @@
-from flask import Flask, make_response, request
-from flask_sqlalchemy import SQLAlchemy
-# from db.config import SQLALCHEMY_DATABASE_URI
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hga50:12345678@restaurant-db.cj7sflyjpv0c.us-west-2.rds.amazonaws.com:5432/restaurant_db'
-db = SQLAlchemy(app)
-
-class Order(db.Model):
-    __tablename__ = 'order'
-
-    order_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Text)
-    amount = db.Column(db.Integer)
-    paid = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return '<User %r>' % self.user_id
-
+from flask import make_response, request
+from Order import Order
+from db.config import app
 
 @app.route('/bill', methods=['GET'])
 def generate_bill():
     user = request.args.get('user_id')
     order = get_user_data(user)
-
+    
     return make_response(order, 200)
-
     
 @app.route('/pay', methods=['GET'])
 def make_payment():
     user = request.args.get('user_id')
     data = Order.query.filter_by(user_id=user).first()
+    
     if data.amount == 0:
         return make_response("The amount value is zero. Cannot pay the bill.", 422)
+    elif data.paid == True:
+        return make_response("The bill has already been paid.", 409)
     else:
         data = Order.query.filter_by(user_id=user).update({Order.paid: True})
         db.session.commit()
         order = get_user_data(user)
 
         return make_response(order, 200)
-
 
 def get_user_data(user):
     data = Order.query.filter_by(user_id=user).first()
@@ -50,6 +35,5 @@ def get_user_data(user):
 
     return order
     
-
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
