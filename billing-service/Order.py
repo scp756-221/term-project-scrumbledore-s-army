c76d1205a416aa35405bb100b3b2db6d505ac448
akg38 <akg38@sfu.ca>
2022-02-19 13:26:35 -0800

    code refactoring

---
+++ billing-service/Order.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from db.config import app
+
+db = SQLAlchemy(app)
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    order_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Text)
+    amount = db.Column(db.Integer)
+    paid = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_id
--- billing-service/app.py
+++ billing-service/app.py
@@ -1,30 +1,12 @@
-from flask import Flask, make_response, request
-from flask_sqlalchemy import SQLAlchemy
-# from db.config import SQLALCHEMY_DATABASE_URI
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hga50:12345678@restaurant-db.cj7sflyjpv0c.us-west-2.rds.amazonaws.com:5432/restaurant_db'
-db = SQLAlchemy(app)
-
-class Order(db.Model):
-    __tablename__ = 'order'
-
-    order_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Text)
-    amount = db.Column(db.Integer)
-    paid = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return '<User %r>' % self.user_id
-
+from flask import make_response, request
+from Order import Order
+from db.config import app
 
 @app.route('/bill', methods=['GET'])
 def generate_bill():
     user = request.args.get('user_id')
     order = get_user_data(user)
-
     return make_response(order, 200)
-
     
 @app.route('/pay', methods=['GET'])
 def make_payment():
@@ -38,9 +20,7 @@ def make_payment():
         data = Order.query.filter_by(user_id=user).update({Order.paid: True})
         db.session.commit()
         order = get_user_data(user)
-
         return make_response(order, 200)
-
 
 def get_user_data(user):
     data = Order.query.filter_by(user_id=user).first()
@@ -49,9 +29,7 @@ def get_user_data(user):
         "amount":data.amount,
         "paid":data.paid
     }
-
     return order
     
-
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
