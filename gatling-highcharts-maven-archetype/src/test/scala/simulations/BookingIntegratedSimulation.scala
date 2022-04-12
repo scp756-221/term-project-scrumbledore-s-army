1547c98f4330bce3a1ca9a19146068304b17a196
akg38 <akg38@sfu.ca>
2022-04-12 00:19:57 -0700

    modified csv for users, minor changes in simulations

--- gatling-highcharts-maven-archetype/src/test/scala/simulations/BookingIntegratedSimulation.scala
+++ gatling-highcharts-maven-archetype/src/test/scala/simulations/BookingIntegratedSimulation.scala
@@ -30,33 +30,37 @@ class BookingIntegratedSimulation extends Simulation {
     .exec(http("get booking")
       .get(s"${URLConstants.booking_service}get_booking")
       .queryParam("booking_id", "${Number}")
-      .check(status is 200))
+      .check(status.saveAs("bookingStatus"))
+      .check(status in (200, 422)))
 
-    .exec(http(requestName = "get menu details")
-      .get("getMenuItems")
-      .check(status is 200))
+    .doIf(session => session("bookingStatus").as[String].equals("200")) {
+      exec(http(requestName = "get menu details")
+        .get("getMenuItems")
+        .check(status is 200))
 
-    .pause(duration = 1)
+      .pause(duration = 1)
 
-    .exec(http(requestName = "take order")
-      .post("takeOrder")
-      .body(StringBody("\"{\\n    \\\"user_id\\\": \\\"${ID}\\\",\\n    \\\"order_list\\\": [\\n        {\\n            \\\"id\\\": 1,\\n            \\\"qty\\\": 2\\n        },\\n        {\\n            \\\"id\\\": 2,\\n            \\\"qty\\\": 5\\n        }\\n    ],\\n    \\\"has_booked\\\": true\\n}\""))
-      .check(status in (200)))
+      .exec(http(requestName = "take order")
+        .post("takeOrder")
+        .body(StringBody("\"{\\n    \\\"user_id\\\": \\\"${ID}\\\",\\n    \\\"order_list\\\": [\\n        {\\n            \\\"id\\\": 1,\\n            \\\"qty\\\": 2\\n        },\\n        {\\n            \\\"id\\\": 2,\\n            \\\"qty\\\": 5\\n        }\\n    ],\\n    \\\"has_booked\\\": true\\n}\""))
+        .check(status in (200)))
 
-    .pause(duration = 1)
+      .pause(duration = 1)
 
-    .exec(http(requestName = "get bill")
-      .get(s"${URLConstants.billing_service}bill")
-      .queryParam("user_id", "${ID}")
-      .check(status is 200))
+      .exec(http(requestName = "get bill")
+        .get(s"${URLConstants.billing_service}bill")
+        .queryParam("user_id", "${ID}")
+        .check(status in(200, 422)))
 
-    .pause(duration = 1)
+      .pause(duration = 1)
 
-    .exec(http("pay bill")
-      .get(s"${URLConstants.billing_service}pay")
-      .queryParam("user_id", "${ID}")
-      .queryParam("booking_id", "${Number}")
-      .check(status in (200, 422, 409)))
+      .exec(http("pay bill")
+        .get(s"${URLConstants.billing_service}pay")
+        .queryParam("user_id", "${ID}")
+        .queryParam("booking_id", "${Number}")
+        .check(status in(200, 422, 409)))
 
-  setUp(fullCycleWoBookingScenario.inject(atOnceUsers(1))).protocols(menuServiceConf)
+    }
+
+  setUp(fullCycleWoBookingScenario.inject(atOnceUsers(5))).protocols(menuServiceConf)
 }
